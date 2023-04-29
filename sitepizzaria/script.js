20b283020b2130372f14bc33bfb670c6bd49a688
FernandoCerqEng19 <fernando11cerqueira@gmail.com>
2023-04-29 05:54:30 -0300

    Add

--- sitepizzaria/script.js
+++ sitepizzaria/script.js
@@ -5,6 +5,8 @@ sr.reveal('.area-1', { duration: 2500 });
 
 sr.reveal('.area-2', { duration: 1000 })
 
+sr.reveal('.container', { duration: 2000, })
+
 sr.reveal('.containerr', { duration: 2000, })
 
 sr.reveal('.containerrr', { duration: 1000, })
